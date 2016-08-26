ad24f3ae703dfd81a76420ee4b5e71b31ccea757
Nir Izraeli <nirizr@gmail.com>
2016-08-26 10:28:58 +0300

    Create task when function data upload is done
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/actions/match.py
+++ idaplugin/rematch/actions/match.py
@@ -14,29 +14,32 @@ class MatchAllAction(base.BoundFileAction):
   group = "Match"
 
   def activate(self, ctx):
-    self.file_id = netnode.bound_file_id
-
     self.function_gen = enumerate(Functions())
-    pd = QtWidgets.QProgressDialog(labelText="Processing functions...",
+    pd = QtWidgets.QProgressDialog(labelText="Processing...\nYou may continue "
+                                             "working but avoid ground-"
+                                             "breaking changes.",
                                    minimum=0, maximum=len(list(Functions())))
     self.progress = pd
     self.progress.canceled.connect(self.cancel)
+
     self.timer = QtCore.QTimer()
-    self.timer.timeout.connect(self.perform)
+    self.timer.timeout.connect(self.perform_upload)
     self.timer.start()
 
-  def perform(self):
+    self.progress.accepted.connect(self.accepted_upload)
+
+  def perform_upload(self):
     try:
       i, offset = self.function_gen.next()
 
-      func = instances.FunctionInstance(self.file_id, offset)
+      func = instances.FunctionInstance(netnode.bound_file_id, offset)
       network.query("POST", "collab/instances/", params=func.serialize(),
                     json=True)
 
       i = i + 1
       self.progress.setValue(i)
       if (i >= self.progress.maximum()):
-        self.timer.stop()
+        self.progress.accept()
     except:
       self.timer.stop()
       raise
@@ -44,7 +47,18 @@ class MatchAllAction(base.BoundFileAction):
   def cancel(self):
     self.timer.stop()
 
+  def accepted_upload(self):
+    self.timer.stop()
+    self.timer.disconnect()
 
+    # TODO: ask for project to compare against
+    task_params = {'action': 'commit', 'file': netnode.bound_file_id,
+                   'project': None}
+    r = network.query("POST", "collab/tasks/", params=task_params, json=True)
+    print(r)
+
+
+# TODO: inherit logic in MatchAllAction
 class MatchFunctionAction(base.BoundFileAction):
   name = "Match &Function"
   group = "Match"
