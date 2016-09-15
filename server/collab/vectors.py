975b184dbf024bd30e4d4dca96d8bfe3599ff29d
Nir Izraeli <nirizr@gmail.com>
2016-09-15 04:19:17 +0300

    implement match for identity functions
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/vectors.py
+++ server/collab/vectors.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Vector:
   @classmethod
   def match(cls, soure, target):
@@ -17,6 +20,20 @@ class DummyVector(Vector):
 class HashVector(Vector):
   id = 'hash'
   name = 'Hash'
+
+  @staticmethod
+  def match(source, target):
+    # unique_values = set(source_dict.values())
+    flipped_rest = defaultdict(list)
+    # TODO: could be optimized by enumerating all identity matchs together
+    for target_id, target_data in target.values_list('id', 'data').iterator():
+      # TODO: could be optimized by uncommenting next line as most 'target'
+      # values won't be present in 'source' list
+      # if v in unique_values:
+      flipped_rest[target_data].append(target_id)
+    for source_id, source_data in source.values_list('id', 'data').iterator():
+      for target_id in flipped_rest.get(source_data, ()):
+        yield (source_id, target_id)
 
 
 class AssemblyHashVector(Vector):
