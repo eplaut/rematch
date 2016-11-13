2a92bf857131f0aba941d9367b35e488624768b3
Nir Izraeli <nirizr@gmail.com>
2016-11-14 01:37:10 +0200

    Add simple 'start server' bash script
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

---
+++ server/start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./manage.py makemigrations collab
+./manage.py migrate
+has_admin=$(echo "select count(*) from auth_user where username='admin';" | ./manage.py dbshell)
+if [ $has_admin -eq 0 ]; then
+    echo "Creating admin super user, please enter password"
+    ./manage.py createsuperuser --username admin --email admin@local.com
+fi
+./manage.py runserver -v 3
