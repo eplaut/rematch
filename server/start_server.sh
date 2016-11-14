6e9b44c701393d3120108a4091c411337cbb7610
Guy <guy@sentinelone.com>
2016-11-14 14:45:55 +0200

    python decl, sqlite3 installation

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+sqlite3_loc=/usr/bin/sqlite3
+if [ ! -f $sqlite3_loc ]; then
+  echo "sqlite3 is not installed, installing.."
+  sudo apt-get install sqlite3
+fi;
 python ./manage.py makemigrations collab
 python ./manage.py migrate
-has_admin=$(echo "select count(*) from auth_user where username='admin';" | ./manage.py dbshell)
+has_admin=$(echo "select count(*) from auth_user where username='admin';" | python ./manage.py dbshell)
 if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
-    ./manage.py createsuperuser --username admin --email admin@local.com
+    python ./manage.py createsuperuser --username admin --email admin@local.com
 fi
-./manage.py runserver -v 3
+python ./manage.py runserver -v 3
