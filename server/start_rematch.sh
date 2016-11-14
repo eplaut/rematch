fffa088b22906544ad72a8dfd54fcc8b896a9645
Guy <guy@sentinelone.com>
2016-11-14 22:07:31 +0200

    introduce start_celery.sh and start_rematch.sh and start_server.sh along with tmux integration (each task gets a diff window)
    start server + tmux now as with sudo

---
+++ server/start_rematch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tmux new-session -s rematch -d ./start_server.sh
+tmux new-window -t rematch -d ./start_celery.sh
+tmux new-window -t rematch
+
+tmux a -t rematch
--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 python ./manage.py makemigrations collab
 python ./manage.py migrate
 has_admin=$(echo "select count(*) from auth_user where username='admin';" | python ./manage.py dbshell)
@@ -7,4 +6,4 @@ if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
     python ./manage.py createsuperuser --username admin --email admin@local.com
 fi
-python ./manage.py runserver -v 3
+sudo python manage.py runserver 192.168.152.138:80
