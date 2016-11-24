2e71fc12f50a929b1d33c6fd958e5318ff2afe71
Nir Izraeli <nirizr@gmail.com>
2016-11-24 19:24:35 +0200

    Improve model creation utilities and fix some tests
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/server/collab/test_all.py
+++ tests/server/collab/test_all.py
@@ -1,9 +1,10 @@
 import pytest
 import json
+from functools import partial
 from rest_framework import status
 
 from django.db import models
-import collab
+from collab.models import Project, File, Task, Instance, Vector
 
 
 collab_models = {'projects': {'name': 'test_project_1', 'private': False,
@@ -16,45 +17,56 @@ collab_models = {'projects': {'name': 'test_project_1', 'private': False,
                  'vectors': {'instance': 1, 'type': 'hash', 'type_version': 0,
                              'data': 'data'}}
 
-collab_model_objects = {'projects': [collab.models.Project(private=False)],
-                        'files': [collab.models.File(name='reqf',
-                                                     description='desc1',
-                                                     md5hash='H' * 32)],
-                        'tasks': ['projects', 'files',
-                                  collab.models.Task(source_file_id=1,
-                                                     target_project_id=1)],
-                        'instances': ['files',
-                                      collab.models.Instance(offset=0,
-                                                             file_id=1)],
-                        'vectors': ['instances',
-                                    collab.models.Vector(instance_id=1,
-                                                         file_id=1,
-                                                         type='hash',
-                                                         type_version=0,
-                                                         data='data')]}
+collab_model_objects = {'projects': partial(Project, private=False),
+                        'files': partial(File, name='name', description='desc',
+                                         md5hash='H' * 32),
+                        'tasks': Task,
+                        'instances': partial(Instance, offset=0),
+                        'vectors': partial(Vector, type='hash', data='data',
+                                          type_version=0)}
 
-collab_model_reqs = {'projects': [],
-                     'files': [],
-                     'tasks': [collab.models.Project(private=False),
-                               collab.models.File()],
-                     'instances': [collab.models.File()],
-                     'vectors': [collab.models.File(name='reqf',
-                                                    description='desc1',
-                                                    md5hash='H' * 32),
-                                 collab.models.Instance(offset=0, file_id=1)]}
+collab_model_reqs = {'projects': {},
+                     'files': {},
+                     'tasks': {'target_project': 'projects',
+                               'source_file': 'files'},
+                     'instances': {'file': 'files'},
+                     'vectors': {'file': 'files',
+                                 'instance': 'instances'}}
 
 
-@pytest.mark.django_db
-def create_models(model_list, user):
-  objects_list = []
-  for model in model_list:
-    if isinstance(model, str):
-      model_list.extend(create_models(collab_model_objects[model], user))
-    else:
-      model.owner = user
-      model.save()
-      objects_list.append(model)
-  return objects_list
+def resolve_reqs(model_name, user):
+  model_reqs = collab_model_reqs[model_name]
+
+  for req_field, req_model in model_reqs.items():
+    obj = collab_model_objects[req_model]()
+
+    create_model(req_model, user, base_obj = obj)
+
+    obj.owner = user
+    obj.save()
+    print("Created model: {} ({}) at {}".format(obj, obj.id, req_field))
+    yield req_field, obj
+
+
+def create_model(model_name, user, base_obj=None):
+  if base_obj is None:
+    base_obj = collab_model_objects[model_name]()
+  base_obj.owner = user
+
+  for req_field, obj in resolve_reqs(model_name, user):
+    base_obj.__setattr__(req_field, obj)
+
+  print(base_obj)
+  return base_obj
+
+def setup_model(model_name, user):
+  model_dict = collab_models[model_name]
+
+  for req_field, obj in resolve_reqs(model_name, user):
+    model_dict[req_field] = obj.id
+
+  print(model_dict)
+  return model_dict
 
 
 def assert_eq(a, b):
@@ -71,9 +83,10 @@ def assert_eq(a, b):
       if d_type == o_type:
         assert d_value == o_value
       else:
-        print("Skipped matching {k}: {d_value}({d_type}) ?? {o_value}({o_type})"
-              "".format(k=k, d_value=d_value, d_type=d_type, o_value=o_value,
-                        o_type=o_type))
+        print("Skipped matching {k}: {d_value}({d_type}) ?? "
+              "{o_value}({o_type})".format(k=k, d_value=d_value,
+                                           d_type=d_type, o_value=o_value,
+                                           o_type=o_type))
 
 
 def assert_response(response, status):
@@ -94,8 +107,8 @@ def test_empty_lists(client, model_name):
 @pytest.mark.parametrize('model_name', collab_models.keys())
 def test_model_guest_list(client, admin_user, model_name):
   # setup objects
-  obj_list = create_models([model_name], admin_user)
-  obj = obj_list[-1]
+  obj = create_model(model_name, admin_user)
+  obj.save()
 
   response = client.get('/collab/{}/'.format(model_name))
   assert_response(response, status.HTTP_200_OK)
@@ -117,7 +130,7 @@ def test_model_guest_creation(client, model_name, model_data):
 @pytest.mark.parametrize('model_name, model_data', collab_models.items())
 def test_model_creation(client, admin_client, admin_user, model_name,
                         model_data):
-  create_models(collab_model_reqs[model_name], admin_user)
+  model_data = setup_model(model_name, admin_user)
 
   response = admin_client.post('/collab/{}/'.format(model_name),
                                data=json.dumps(model_data),
